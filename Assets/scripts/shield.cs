3a369f3ea0a9955d16b67421c62c8e26d58a5cba
serendipity0813 <91657737+serendipity0813@users.noreply.github.com>
2023-12-28 12:32:26 +0900

    Move

---
+++ Assets/scripts/shield.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //마우스 위치 따와서 x, y 좌표 변경하기
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = new Vector3(mousePos.x, mousePos.y, 0);
+    }
+}
